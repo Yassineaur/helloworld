55c153b4a8fef7b2718463f5e433a6a21cdd61f3
Yassine <Apple@192.168.1.4>
2024-07-06 00:43:27 +0100

    hhhhh

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World  !');
+  res.send('Hello World!');
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error', err);
+  process.exit(1); // Mandatory (as per the Node.js docs)
 });
